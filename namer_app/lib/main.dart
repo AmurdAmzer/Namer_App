3f0b76b8bb79da77617cfe996e0020511d2591c3
AmurdAmzer <amurdamzer@gmail.com>
2024-06-07 13:07:49 -0400

    adding changes to github

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text(' AhmedHamza A random idea:'),
+          Text('A random idea:'),
           Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
