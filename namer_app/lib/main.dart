fb530856d39cd9cc36d87035413c610643caca8b
AmurdAmzer <amurdamzer@gmail.com>
2024-06-11 01:51:31 -0400

    adding changes to github

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -31,6 +31,17 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
